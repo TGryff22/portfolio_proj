c81545b1a88b2cacfa257f139750ed30eb6757b7
Tre <trevonb212@gmail.com>
2023-05-01 17:29:30 -1000

    insomnia_update

--- app/breeds/models.py
+++ app/breeds/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Breed(models.Model):
     title = models.CharField(max_length=70, blank=False, default="")
-    tutorial_url = models.CharField(max_length=200, blank=False, default="")
+    breed_url = models.CharField(max_length=200, blank=False, default="")
     image_path = models.CharField(max_length=150, blank=True, null=True)
     description = models.CharField(max_length=200, blank=False, default="")
     published = models.BooleanField(default=False)
